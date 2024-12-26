ad1cd2df378a83cc167309852ca186823fb65bed
joginders529 <109283559+joginders529@users.noreply.github.com>
2024-12-26 18:08:27 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,18 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { Button } from "react-bootstrap";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>This is App Page</h1>
+      <div className="App">
+        <h1>This is App Page</h1>
+        <button>Normal Button</button>
+        <Button>Bootstrap Button</Button>
+      </div>
     </>
   );
 }
